fff8ee130638d41bef228804713cd102f151c7bf
Visalatchi thangaraj <visalatchithangaraj2005@gmail.com>
2026-07-07 10:41:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from PIL import Image
 from datetime import datetime
 import os
+from predict import predict_image
 
 # -------------------------------------------------
 # PAGE CONFIGURATION
